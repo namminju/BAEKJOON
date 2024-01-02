02772fc92411cf4468874f15eac34a37f0442719
MJ <98680218+namminju@users.noreply.github.com>
2024-01-02 21:48:28 +0900

    [Silver II] Title: 생태학, Time: 480 ms, Memory: 2164 KB -BaekjoonHub

---
+++ 백준/Silver/4358. 생태학/생태학.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+int main() {
+    string name;
+    map<string,int> tree;
+    int whole = 0;
+    while (getline(cin, name) && !name.empty()) {
+        whole++;
+        if (!tree[name]) { tree[name] = 1; }
+        else { tree[name]++; }
+    }
+
+    for (auto iter : tree) {
+        cout.precision(4);
+        cout << fixed;
+        cout << iter.first << " " << ((double)iter.second / whole) * 100 << "\n";
+    }
+    return 0;
+}
