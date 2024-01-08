c7c74effb66b21a99521897cedca7b60f38e5e98
MJ <98680218+namminju@users.noreply.github.com>
2024-01-08 18:07:57 +0900

    Update 요세푸스 문제.cc

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -5,29 +5,29 @@
 using namespace std;
 
 int main() {
-    int n=0, k=0;
+    int n=0, k=0;//입력 받을 변수
 
-    cin >> n;
+    cin >> n; // N 입력받기
     vector<int> N;
     for (int i = 0; i < n; i++) {
-        N.push_back(i + 1);
+        N.push_back(i + 1); //N개 만큼 수를 차례대로 넣기
     }
-    cin >> k;
-    int temp = k-1;
-    cout << "<";
-    while(N.size()!=0){
-        n--;
-        if (n != 0) {
-            cout << N.at(temp) << ", ";
-            N.erase(N.begin() + temp);
-            temp += k - 1;
-            temp %= n;
+    cin >> k;//K 입력받기
+    int temp = k-1;//현재 제거 대상의 번호
+    cout << "<";//출력 형식 맞추기
+    while(N.size()!=0){//N 벡터가 0이 아닐때만(즉, 사람이 존재하지 않으면 멈춤)
+        n--;//n의 크기를 줄이기(원형이기 때문에 범위를 넘어가면 n으로 나눈 나머지가 현재 제거 대상의 번호이므로)
+        if (n != 0) {//마지막 사람이 아니면
+            cout << N.at(temp) << ", ";//현재 제거 대상 번호를 출력
+            N.erase(N.begin() + temp);//대상 제거
+            temp += k - 1;//제거 대상 번호 업데이트
+            temp %= n;//범위가 넘어갈 때를 대비하여 제거 대상 번호 업데이트(원형이므로!!)
         }
-        else {
-            cout << N.at(temp);
-            N.erase(N.begin() + temp);
+        else {//마지막 사람에 대해(',' 제거)
+            cout << N.at(temp);//현재 제거 대상 번호를 출력
+            N.erase(N.begin() + temp);//대상 제거
         }
     }
-    cout << ">";
-    return 0;
+    cout << ">";//출력 형식 맞추기
+    return 0;//종료
 }
