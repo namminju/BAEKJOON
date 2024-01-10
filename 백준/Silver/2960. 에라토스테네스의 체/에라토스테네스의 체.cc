4b18e2aec535f750d6a95990f4526e284008e2bf
MJ <98680218+namminju@users.noreply.github.com>
2024-01-10 20:10:15 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+int main() {
+    cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int n, k;
+    cin >> n >> k;
+    map<int, bool>num;
+    for (int i = 2; i <= n; i++) {
+        num[i] = true;
+    }
+    int now = 0;
+    while (k != now) {
+        int p = 0;
+        for (int i = 2; i <= n; i++) {
+            if (num[i]) {
+                p = i;
+                break;
+            }
+        }
+        for (int i = 1; i * p <= n; i++) {
+            if (num[i * p]) {
+                num[i * p] = false;
+                now++;
+                if (now == k) { cout << i * p; break; }
+            }
+        }
+    }
+    return 0;
+}
