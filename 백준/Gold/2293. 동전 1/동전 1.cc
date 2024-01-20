7490449b0c38038c4675865da92f15d2551da07f
MJ <98680218+namminju@users.noreply.github.com>
2024-01-20 21:56:44 +0900

    [Gold V] Title: 동전 1, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <list>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+
+    int n, sum;
+    cin >> n >> sum;
+    int* dp = new int[sum + 1];
+    for (int i = 1; i <= sum; i++) {
+        dp[i] = 0;
+    }
+    int value;
+    dp[0] = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> value;
+        for (int i = 1; i <= sum; i++)
+        {
+            if (i >= value)
+            {
+                dp[i] = dp[i] + dp[i - value];
+            }
+        }
+    } 
+    cout << dp[sum];
+    return 0;
+}
