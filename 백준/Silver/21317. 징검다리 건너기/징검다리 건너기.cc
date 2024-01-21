331d039c6b0aae0fa3018df0b4cce33c01d1401e
MJ <98680218+namminju@users.noreply.github.com>
2024-01-22 07:32:36 +0900

    [Silver I] Title: 징검다리 건너기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/21317. 징검다리 건너기/징검다리 건너기.cc
+++ 백준/Silver/21317. 징검다리 건너기/징검다리 건너기.cc
@@ -8,43 +8,38 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int N, K;
-    cin >> N;
+    int n, K;
+    cin >> n;
 
-    if (N == 1) {
-        cin >> K;
-        cout << 0;
+    vector<pair<int, int>> energy(n);
+    vector<vector<int>> dp(n + 1, vector<int>(2, INT_MAX));
+    dp[1][0] = 0;
+    dp[1][1] = 0;
+
+    for (int i = 1; i < n; ++i) {
+        cin >> energy[i].first >> energy[i].second;
     }
-    else {
-        vector<pair<int, int>> energy(N);
-        vector<vector<int>> dp(N + 1, vector<int>(2, INT_MAX));
-        dp[1][0] = 0;
-        dp[1][1] = 0;
+    cin >> K;
 
-        for (int i = 1; i < N; ++i) {
-            cin >> energy[i].first >> energy[i].second;
-        }
-        cin >> K;
-
-        for (int i = 1; i <= N - 1; ++i) {
-            if (i + 1 <= N) {
-                dp[i + 1][0] = min(dp[i + 1][0], dp[i][0] + energy[i].first);
-                dp[i + 1][1] = min(dp[i + 1][1], dp[i][1] + energy[i].first);
-            }
-
-            if (i + 2 <= N) {
-                dp[i + 2][0] = min(dp[i + 2][0], dp[i][0] + energy[i].second);
-                dp[i + 2][1] = min(dp[i + 2][1], dp[i][1] + energy[i].second);
-            }
-
-            if (i + 3 <= N) {
-                dp[i + 3][1] = min(dp[i + 3][1], dp[i][0] + K);
-            }
+    for (int i = 1; i <= n - 1; ++i) {
+        if (i + 1 <= n) {
+            dp[i + 1][0] = min(dp[i + 1][0], dp[i][0] + energy[i].first);
+            dp[i + 1][1] = min(dp[i + 1][1], dp[i][1] + energy[i].first);
         }
 
-        int minEnergy = min(dp[N][0], dp[N][1]);
-        cout << minEnergy;
+        if (i + 2 <= n) {
+            dp[i + 2][0] = min(dp[i + 2][0], dp[i][0] + energy[i].second);
+            dp[i + 2][1] = min(dp[i + 2][1], dp[i][1] + energy[i].second);
+        }
+
+        if (i + 3 <= n) {
+            dp[i + 3][1] = min(dp[i + 3][1], dp[i][0] + K);
+        }
     }
+
+    int minEnergy = min(dp[n][0], dp[n][1]);
+    cout << minEnergy;
+    
 
     return 0;
 }
