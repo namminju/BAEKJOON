a043b0fc6bc7abd41dca8261588f842a4b3d3ff5
MinJu <98680218+namminju@users.noreply.github.com>
2025-05-26 14:25:46 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 1.66 ms, Memory: 84.5 MB -BaekjoonHub

--- Java/프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ Java/프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,46 +1,37 @@
 import java.util.*;
 class Solution {
-    public Map<Integer, List<Integer>> tree = new HashMap<>();
-    int[] child;
+    int[] childNum;
     boolean[] visited;
-    
-    public int dfs(int n){
+    Map<Integer, List<Integer>> map = new HashMap<>();
+    public int getChildNum(int n){
         int count = 1;
         visited[n]=true;
-        
-        for(int num : tree.get(n)){
-            if(!visited[num]){
-                count+=dfs(num);
+        for(int c: map.getOrDefault(n, new ArrayList<>())){
+            if(!visited[c]){                
+                count += getChildNum(c);
             }
-           
         }
-        child[n] = count; 
+        childNum[n] = count;
         return count;
     }
-    
     public int solution(int n, int[][] wires) {
         int answer = n;
-        
-        child = new int[n+1];
+        childNum = new int[n+1];
         visited = new boolean[n+1];
-        
-        for(int i=0;i<wires.length;i++){
-            List<Integer> list = tree.getOrDefault(wires[i][0], new ArrayList<>());
-            list.add(wires[i][1]);
-            tree.put(wires[i][0], list);
+        for(int w[]: wires){
+            List<Integer> list = map.getOrDefault(w[0], new ArrayList<>());
+            list.add(w[1]);
+            map.put(w[0], list);
             
-            list = tree.getOrDefault(wires[i][1], new ArrayList<>());
-            list.add(wires[i][0]);
-            tree.put(wires[i][1], list);
+            list = map.getOrDefault(w[1], new ArrayList<>());
+            list.add(w[0]);
+            map.put(w[1], list);
         }
-        
-        dfs(1);
-        
-        
-        for(int i=2;i<child.length;i++){
-            int a = child[1]-child[i];
-            int b = child[i];
-            answer = Math.min(answer, Math.abs(a-b));
+        getChildNum(1);
+   
+        for(int i: childNum){
+            int value = Math.abs((n-i) - i);
+            answer = Math.min(answer, value);
         }
         return answer;
     }
