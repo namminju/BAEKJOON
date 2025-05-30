b9d4a4afa82bfa6af18739e66e6047b32e13ce75
MinJu <98680218+namminju@users.noreply.github.com>
2025-05-30 10:09:05 +0900

    [level 3] Title: 단어 변환, Time: 1.06 ms, Memory: 82.7 MB -BaekjoonHub

--- Java/프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ Java/프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,9 +1,10 @@
 import java.util.*;
 
 class Solution {
+   
     public int diffNum(String begin, String target){
         int count = 0;
-        for(int i=0;i<begin.length();i++){
+        for(int i=0;i<begin.length(); i++){
             if(begin.charAt(i)!=target.charAt(i)){
                 count++;
             }
@@ -11,31 +12,36 @@ class Solution {
         return count;
     }
     public class Node{
-        public String word;
-        public int depth;
+        String str;
+        int cnt;
         
-        Node(String word, int depth){
-            this.word = word;
-            this.depth = depth;
+        Node(String str, int cnt){
+            this.str = str;
+            this.cnt = cnt;
         }
     }
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
-        Map<String, Boolean> visited = new HashMap<>();
+        
         Deque<Node> deque = new ArrayDeque<>();
+        Map<String, Boolean> visited = new HashMap<>();
         
         deque.addLast(new Node(begin, 0));
         visited.put(begin, true);
         
         while(!deque.isEmpty()){
             Node cur = deque.removeFirst();
-            if(cur.word.equals(target)){
-                answer = cur.depth;
+            
+            if(cur.str.equals(target)){
+                answer = cur.cnt;
+                break;
             }
-            for(String w: words){
-                if(diffNum(cur.word, w)==1 && !visited.getOrDefault(w, false)){
-                    deque.addLast(new Node(w, cur.depth+1));
-                    visited.put(w, true);
+            
+            for(String word: words){
+                if(!visited.getOrDefault(word, false)
+                    && diffNum(cur.str, word)==1){
+                    deque.addLast(new Node(word, cur.cnt + 1));
+                    visited.put(word, true);
                 }
             }
         }
