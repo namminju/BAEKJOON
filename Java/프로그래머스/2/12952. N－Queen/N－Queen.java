4f0be6131082e8609a5ead86b56d7d2e74427873
MinJu <98680218+namminju@users.noreply.github.com>
2025-07-04 13:07:13 +0900

    [level 2] Title: N-Queen, Time: 291.91 ms, Memory: 90 MB -BaekjoonHub

--- Java/프로그래머스/2/12952. N－Queen/N－Queen.java
+++ Java/프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -1,52 +1,52 @@
 class Solution {
+    int answer = 0;
     boolean[][] visited;
-    int count=0;
+    public void dfs(int n, int x){
+        if(x==n){
+            answer++;
+            return;
+        }
+        for(int i=0;i<n;i++){
+            if(isPossible(x, i, n)){
+                visited[x][i] = true;
+                dfs(n, x+1);
+                visited[x][i] = false;
+            }
+            
+        }
+    }
     public boolean isPossible(int x, int y, int n){
         for(int i=0;i<n;i++){
             if(visited[x][i] || visited[i][y]){
                 return false;
             }
         }
-        for(int i=1;x-i>=0 && y-i>=0;i++){//왼 위
+        for(int i=0; x-i>=0 && y-i>=0 ;i++){
             if(visited[x-i][y-i]){
                 return false;
             }
         }
-        for(int i=1;x+i<n && y+i<n;i++){//오 아래
-            if(visited[x+i][y+i]){
-                return false;
-            }
-        }
-        for(int i=1;x+i<n && y-i>=0;i++){//오 위
-            if(visited[x+i][y-i]){
-                return false;
-            }
-        }
-        for(int i=1;x-i>=0 && y+i<n;i++){//왼 아래
+        for(int i=0; x-i>=0 && y+i<n ;i++){
             if(visited[x-i][y+i]){
                 return false;
             }
         }
-        return  true;
-    }
-    public void dfs(int n, int x){
-        if(x==n){
-            count++;
-            return;
+        for(int i=0;x+i<n && y-i>=0 ;i++){
+            if(visited[x+i][y-i]){
+                return false;
+            }
         }
-        for(int i=0;i<n;i++){
-            if(isPossible(x, i, n)){//놓을 수 있으면
-                visited[x][i] = true;
-                dfs(n, x+1);
-                visited[x][i] = false;
-            }       
+        for(int i=0;x+i<n && y+i<n ;i++){
+            if(visited[x+i][y+i]){
+                return false;
+            }
         }
+        return true;
 
     }
     public int solution(int n) {
-        int answer = 0;
         visited = new boolean[n][n];
         dfs(n, 0);
-        return count;
+        return answer;
     }
 }
