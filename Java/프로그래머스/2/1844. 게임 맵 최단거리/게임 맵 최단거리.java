dd4be5682a0f5dc80f0b3162f6c2f96c58149636
MinJu <98680218+namminju@users.noreply.github.com>
2025-09-26 21:28:52 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.80 ms, Memory: 55.3 MB -BaekjoonHub

--- Java/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ Java/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,25 +1,28 @@
-import java.util.*; 
+import java.util.*;
 
 class Solution {
-    public static final int[] dx = {0, 0, 1, -1};
-    public static final int[] dy = {1, -1, 0, 0};
-    
+    public int[] dx = {0, 0, -1, 1};
+    public int[] dy = {-1, 1, 0, 0};
+    public boolean[][] visited;
     public int solution(int[][] maps) {
         int answer = -1;
         int n = maps.length;
         int m = maps[0].length;
         
-        boolean[][] visited = new boolean[n][m];
+        visited = new boolean[n][m];
         
         Deque<int[]> deque = new ArrayDeque<>();
-        
-        deque.addLast(new int[]{0, 0, 1});
+   
+        //x,y, 이동 거리
+        deque.add(new int[]{0, 0, 1});
         visited[0][0] = true;
-                      
+        
         while(!deque.isEmpty()){
             int[] cur = deque.removeFirst();
             
-            if(cur[0]==n-1 && cur[1]==m-1){
+            //System.out.println(cur[0]+"," +cur[1]+": "+cur[2]);
+            
+            if(cur[0] == n-1 && cur[1] == m-1){
                 answer = cur[2];
                 break;
             }
@@ -28,18 +31,22 @@ class Solution {
                 int nx = cur[0] + dx[i];
                 int ny = cur[1] + dy[i];
                 
-                if(nx<0 || ny<0 || nx>=n || ny>=m){
+                if(nx<0 || nx>=n || ny<0 || ny>=m){
                     continue;
                 }
                 
-                if(maps[nx][ny]==1 && !visited[nx][ny]){
+                if(!visited[nx][ny] && maps[nx][ny]==1){
                     deque.addLast(new int[]{nx, ny, cur[2]+1});
-                    visited[nx][ny]=true;
+                    visited[nx][ny] = true;
                 }
             }
-            
         }
-        
         return answer;
     }
 }
+
+// [1, 0, 1, 1, 1]
+// [1, 0, 1, 0, 1]
+// [1, 0, 1, 1, 1]
+// [1, 1, 1, 0, 1]
+// [0, 0, 0, 0, 1]
