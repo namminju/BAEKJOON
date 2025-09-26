53ac08536b1825276ba9139fede1522089017f73
MinJu <98680218+namminju@users.noreply.github.com>
2025-09-26 22:03:27 +0900

    [level 3] Title: 단어 변환, Time: 0.89 ms, Memory: 88.1 MB -BaekjoonHub

--- Java/프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ Java/프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,50 +1,50 @@
 import java.util.*;
 
 class Solution {
-   
-    public int diffNum(String begin, String target){
+    public class Word{
+        public String word;
+        public int deg;
+        
+        public Word(String word, int deg){
+            this.word = word;
+            this.deg = deg;
+        }
+    }
+    public int diff(String word, String target){
         int count = 0;
-        for(int i=0;i<begin.length(); i++){
-            if(begin.charAt(i)!=target.charAt(i)){
+        for(int i=0;i<word.length();i++){
+            if(word.charAt(i)!=target.charAt(i)){
                 count++;
             }
         }
         return count;
     }
-    public class Node{
-        String str;
-        int cnt;
-        
-        Node(String str, int cnt){
-            this.str = str;
-            this.cnt = cnt;
-        }
-    }
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
         
-        Deque<Node> deque = new ArrayDeque<>();
-        Map<String, Boolean> visited = new HashMap<>();
+        Map<String, Boolean> map = new HashMap<>();
+        Deque<Word> deque = new ArrayDeque<>();
         
-        deque.addLast(new Node(begin, 0));
-        visited.put(begin, true);
+        deque.add(new Word(begin, 0));
+        map.put(begin, true);
         
         while(!deque.isEmpty()){
-            Node cur = deque.removeFirst();
+            Word cur = deque.removeFirst();
             
-            if(cur.str.equals(target)){
-                answer = cur.cnt;
+ 
+            if(cur.word.equals(target)){
+                answer = cur.deg;
                 break;
             }
             
-            for(String word: words){
-                if(!visited.getOrDefault(word, false)
-                    && diffNum(cur.str, word)==1){
-                    deque.addLast(new Node(word, cur.cnt + 1));
-                    visited.put(word, true);
+            for(String word : words){
+                if(!map.getOrDefault(word, false) && diff(cur.word, word) == 1){
+                    deque.addLast(new Word(word, cur.deg+1));
+                    map.put(word, true);
                 }
             }
         }
+        
         
         return answer;
     }
