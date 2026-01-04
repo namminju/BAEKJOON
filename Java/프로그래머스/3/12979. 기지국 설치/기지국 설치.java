bcb9e0343192ecb40ca4931b3a6b8652ba2a0c06
MinJu <98680218+namminju@users.noreply.github.com>
2026-01-04 17:17:34 +0900

    [level 3] Title: 기지국 설치, Time: 0.56 ms, Memory: 54.7 MB -BaekjoonHub

--- Java/프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ Java/프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -3,23 +3,24 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[] stations, int w) {
         int answer = 0;
-
-        //Arrays.sort(stations);
-        List<Integer> gap = new ArrayList<>();
-
-        int i = 1;
-        for (int s : stations) {
-            int left = s - w;
-            if (left > i) gap.add(left - i);
-            i = s + w + 1;
-        }
-
-        if (i <= n) gap.add(n - i + 1);
-
         int cover = 2 * w + 1;
-        for (int g : gap) {
-            answer += (g + cover - 1) / cover;
+
+        int idx = 1; // 아직 커버되지 않은 가장 왼쪽(1-based)
+
+        for (int s : stations) {
+            int left = s - w;          // 현재 기지국 커버 시작
+            if (left > idx) {          // 빈 구간 존재
+                int gap = left - idx;
+                answer += (gap + cover - 1) / cover;
+            }
+            idx = s + w + 1;           // 다음 미커버 시작점
         }
+
+        if (idx <= n) {                // 마지막 꼬리 구간
+            int gap = n - idx + 1;
+            answer += (gap + cover - 1) / cover;
+        }
+
         return answer;
     }
 }
