297df1e010c21dd3ae0f55a09b976dc0e88e3e83
MinJu <98680218+namminju@users.noreply.github.com>
2026-01-04 15:50:35 +0900

    [level 3] Title: 네트워크, Time: 3.32 ms, Memory: 79.9 MB -BaekjoonHub

--- Java/프로그래머스/3/43162. 네트워크/README.md
+++ Java/프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
 
 ### 성능 요약
 
-메모리: 81.1 MB, 시간: 2.07 ms
+메모리: 79.9 MB, 시간: 3.32 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 30일 21:48:05
+2026년 01월 04일 15:50:33
 
 ### 문제 설명
 
--- Java/프로그래머스/3/43162. 네트워크/네트워크.java
+++ Java/프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,39 +1,39 @@
 import java.util.*;
 
 class Solution {
-    public boolean[] visited = new boolean[201];
-    public Map<Integer, List<Integer>> map = new HashMap<>();
+    Map<Integer, List<Integer>> map = new HashMap<>();
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        for(int i=0; i<n;i++){
-             for(int j=i; j<n;j++){
-                 if(computers[i][j]==1){
-                     map.computeIfAbsent(i, k->new ArrayList<>()).add(j);
-                     map.computeIfAbsent(j, k->new ArrayList<>()).add(i);
-                 }
-             }
+        boolean[] visited = new boolean[n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){ 
+                if(computers[i][j] == 1){ 
+                    map.computeIfAbsent(j, k-> new ArrayList<>()).add(i);
+                    map.computeIfAbsent(i, k-> new ArrayList<>()).add(j);
+                }
+            }
         }
         
-        for(int i=0; i<n;i++){
-            if(!visited[i]){
-                 visited[i]=true;
-                answer++;
-                Deque<Integer> deque = new ArrayDeque<>();
-                deque.add(i);
-                
-                while(!deque.isEmpty()){
-                    int cur = deque.remove();
-                 
-                    for(int v: map.getOrDefault(cur, new ArrayList<>())){
-                        if(visited[v]){
-                            continue;
-                        }
-                        visited[v]=true;
-                        deque.add(v);
+        for(int i=0;i<n;i++){
+            if(visited[i]) continue;
+            answer++;
+            
+            Deque<Integer> dq = new ArrayDeque<>(); 
+            dq.add(i);
+            visited[i] = true;
+             
+            while(!dq.isEmpty()){
+                int cur = dq.remove(); 
+                for(int next: map.getOrDefault(cur, new ArrayList<>())){
+                    if(!visited[next]){
+                        dq.add(next);
+                        visited[next] = true;
+                        System.out.println(next);
                     }
                 }
             }
         }
+        
         return answer;
     }
 }
