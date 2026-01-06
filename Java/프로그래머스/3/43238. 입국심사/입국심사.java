955f544ff4c49a551e8081f8a15c60a6d24ce687
MinJu <98680218+namminju@users.noreply.github.com>
2026-01-06 18:44:35 +0900

    [level 3] Title: 입국심사, Time: 96.10 ms, Memory: 93.2 MB -BaekjoonHub

---
+++ Java/프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] times) {
+        Arrays.sort(times); 
+        long left = 0;
+        long right = (long)times[times.length-1]*n;
+        long answer = 0;
+        
+        while(left!=right){
+            long mid = (left+right)/2;
+            long cnt = 0;
+            
+            for(int t: times){
+                cnt+=mid/t;
+                if(cnt>n) break;
+            }
+                        
+            if(cnt>=n){
+                right = mid;
+            }else{
+                left = mid+1;
+            }
+        }
+        
+    
+        return left;
+    }
+}
